59f28d1d5c98f37c432985d650d66147bdfb629c
ycs18153 <twl223025@gmail.com>
2021-07-20 17:11:02 +0800

    rollback

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const Todo = require('./src/model');
 const line = require('@line/bot-sdk');
-var express = require('express');
+const express = require('express');
 const config = {
     channelAccessToken: 'vkB5D5SxgqQxP1xGeSCs7+//PyCFtx+t+bu41aiCdo3Ty8cu8NPh24/BbByGkQJ91KSuY1cOtI55EOntGm4lBQsTMe9e6Cr+K9nyDX8S+SRpfvnJjtp2eQClPl2XeyQ/n1W2WJtJzD5OVNUKH5HMtgdB04t89/1O/w1cDnyilFU=',
     channelSecret: '76a2d94b3ed2c7e0ed33d5700c1883a5'
