7d31902489bdd9636ab57c3aec4fd82700097650
ycs18153 <twl223025@gmail.com>
2021-07-20 11:27:30 +0800

    rollback

--- index.js
+++ index.js
@@ -40,11 +40,10 @@ function handleEvent(event) {
     return client.replyMessage(event.replyToken, echo);
 }
 
-//#region Listening
+//#region Listening handler
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
 //#endregion
-
