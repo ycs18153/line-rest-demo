cce754a56f531021a3cbd16c8f8e6128f24489e5
ycs18153 <twl223025@gmail.com>
2021-07-20 11:26:09 +0800

    rollback

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const line = require('@line/bot-sdk');
 var express = require('express');
-const mongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const ObjectID = require('mongodb').ObjectID;
-const router = require('./restAPI');
 const config = {
     channelAccessToken: 'vkB5D5SxgqQxP1xGeSCs7+//PyCFtx+t+bu41aiCdo3Ty8cu8NPh24/BbByGkQJ91KSuY1cOtI55EOntGm4lBQsTMe9e6Cr+K9nyDX8S+SRpfvnJjtp2eQClPl2XeyQ/n1W2WJtJzD5OVNUKH5HMtgdB04t89/1O/w1cDnyilFU=',
     channelSecret: '76a2d94b3ed2c7e0ed33d5700c1883a5'
@@ -14,7 +10,7 @@ const client = new line.Client(config);
 
 // create Express app
 const app = express();
-app.use(bodyParser.json(), apis);
+app.use(bodyParser.json());
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
