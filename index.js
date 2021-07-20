ce4289177a3bf7450ae857060d287f8d43f9d3bd
ycs18153 <twl223025@gmail.com>
2021-07-20 22:19:59 +0800

    remove

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
+'use strict';
+
 const line = require('@line/bot-sdk');
 const express = require('express');
-const bodyParser = require('body-parser');
 
-const router = require('./src/api/routes/todoRoute');
+// create LINE SDK config from env variables
 const config = {
     channelAccessToken: 'vkB5D5SxgqQxP1xGeSCs7+//PyCFtx+t+bu41aiCdo3Ty8cu8NPh24/BbByGkQJ91KSuY1cOtI55EOntGm4lBQsTMe9e6Cr+K9nyDX8S+SRpfvnJjtp2eQClPl2XeyQ/n1W2WJtJzD5OVNUKH5HMtgdB04t89/1O/w1cDnyilFU=',
     channelSecret: '76a2d94b3ed2c7e0ed33d5700c1883a5'
 };
 
-// Import DB Connection
-// require("./src/config/db");
-
 // create LINE SDK client
 const client = new line.Client(config);
 
 // create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
-
-// use bodyParser middleware on express app
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Add endpoint
-app.get('/', (req, res) => {
-    res.send("hello world");
-})
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-    console.log(req, res)
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
@@ -39,38 +28,22 @@ app.post('/callback', line.middleware(config), (req, res) => {
         });
 });
 
-
-
 // event handler
 function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
-        console.log("hohohohohoohhohohohohoohhohohohohoohhohohohohoohhohohohohoohhohohohohooh");
         // ignore non-text-message event
         return Promise.resolve(null);
     }
 
-    // if (event.message.text == 'ls') {
-    //     const echo = {
-    //         type: 'text',
-    //         text: 'show todo list'
-    //     };
-    //     return client.replyMessage(event.replyToken, echo);
-    // }
-    else {
-        // create a echoing text message
-        const echo = {
-            type: 'text',
-            text: event.message.text
-        };
-        // use reply API
-        return client.replyMessage(event.replyToken, echo);
-    }
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
 }
 
-//#region Listening handler
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
-//#endregion
