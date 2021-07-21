5da43caa405cb26f02536aa80f018838cdecd16b
ycs18153 <twl223025@gmail.com>
2021-07-21 18:01:07 +0800

    convert from js to py

--- src/api/routes/todoRoute.js
+++ src/api/routes/todoRoute.js
@@ -2,29 +2,30 @@ const express = require('express');
 const mongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
+const todoModel = require('../models/todoModel');
 
-const router = express();
-
-const port = 8000;
+const router = express.Router();
 router.use(bodyParser.json());
 
-const url = 'mongodb+srv://admin:admin@cluster0.gitsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-mongoClient.connect(url, (err, client) => {
-    if (err) return console.log(err)
-    db = client.db('myFirstDatabase')
-    router.listen(port, () => {
-        console.log('listening on 3000')
-    })
+var database, collection;
+const url = 'mongodb+srv://admin:admin@cluster0.gitsk.mongodb.net/myFirstDatabase?retryWrites=true';
+mongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if (err) return console.log(err);
+    database = client.db('myFirstDatabase');
+    collection = database.collection('todoList');
+    console.log(`Connect to ${database}, and the collection here is ${collection}`);
 })
 
 /* 
  * Post Method
  * Note: the sample request is provided above (region).   
  */
-router.post('/todo', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body); //your json
-    db.collection('todoList').save(req.body, (err, result) => {
+    database.collection('todo').save(req.body, (err, result) => {
         if (err) return console.log(err);
         console.log('saved to mongoDB');
         res.send(req.body);
@@ -32,19 +33,19 @@ router.post('/todo', (req, res) => {
 })
 
 /* Get Method */
-router.get('/todo', (req, res) => {
-    db.collection('todoList').find().toArray((err, result) => {
+router.get('/', (req, res) => {
+    database.collection('todo').find().toArray((err, result) => {
         if (err) throw err;
         console.log('GET successful!');
         res.send({ data: result });
     })
-})
+});
 
 /* Delete Method */
-router.delete('/todo/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     // use _id need use ObjectID(value)
     const obj = { _id: ObjectID(req.params.id) };
-    db.collection('todoList').remove(obj, (err, result) => {
+    database.collection('todo').remove(obj, (err, result) => {
         if (err) throw err;
         console.log('1 document deleted');
         res.send('delete success');
@@ -52,20 +53,15 @@ router.delete('/todo/:id', (req, res) => {
 })
 
 /* Put Method */
-router.put('/todo/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     console.log(req.params.id, req.body);
     const newvalues = { $set: req.body };
     const obj = { _id: ObjectID(req.params.id) };
-    db.collection('todoList').updateOne(obj, newvalues, (err, result) => {
+    database.collection('todo').updateOne(obj, newvalues, (err, result) => {
         if (err) throw err;
         console.log("1 document update");
         res.send('update success');
     })
 })
-
-/* Rendering homepage. */
-router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
 
 module.exports = router;
