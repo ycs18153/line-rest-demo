a34ff13a36327c74c252b903765416990e20e9bd
ycs18153 <twl223025@gmail.com>
2021-07-21 18:20:52 +0800

    py echo bot v4

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
+# access_token & chanel_id
 line_bot_api = LineBotApi(
     'vkB5D5SxgqQxP1xGeSCs7+//PyCFtx+t+bu41aiCdo3Ty8cu8NPh24/BbByGkQJ91KSuY1cOtI55EOntGm4lBQsTMe9e6Cr+K9nyDX8S+SRpfvnJjtp2eQClPl2XeyQ/n1W2WJtJzD5OVNUKH5HMtgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('76a2d94b3ed2c7e0ed33d5700c1883a5')
