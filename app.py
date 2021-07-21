a4946b5d13db90fba87c55100f49045339d31b23
ycs18153 <twl223025@gmail.com>
2021-07-21 20:47:11 +0800

    bot interact with mongoDB via restful api

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 import configparser
-
+import requests
 import random
+
+from requests.exceptions import SSLError
 
 app = Flask(__name__)
 
@@ -36,15 +38,71 @@ def callback():
 
     return 'OK'
 
-# Echo
+
+helpMessage = "`create task <task_name>` to create a task.\n" \
+    + "`read tasks` to get all the tasks you created.\n" \
+    + "`update task <task_id>` to update the content of the task.\n" \
+    + "`delete task <task_id>` to remove the task.\n" \
+    + "`get id` to get the task's ID for operations later."
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    if "read tasks" in event.message.text:
+        try:
+            response = requests.get("http://localhost:8000/task/")
+            res = ""
+            for item in response.json():
+                res += '*' + item['name'] + '\n'
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text=res))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="something wents wrong, please retry..."))
+    # elif "update task" in event.message.text:
+    #     try:
+    #         taskID = event.message.text.split("task")[1].strip()
+    #         requests.put("http://localhost:8000/task/%s" % taskID, )
+
+    #     except:
+    #         line_bot_api.reply_message(
+    #             event.reply_token, TextSendMessage(text="something wents wrong, please retry..."))
+    elif "delete task" in event.message.text:
+        try:
+            taskID = event.message.text.split("task")[1].strip()
+            requests.delete("http://localhost:8000/task/%s" % taskID)
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="delete success!"))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="something wents wrong, please retry..."))
+
+    elif "create task" in event.message.text:
+        try:
+            taskName = event.message.text.split("task")[1].strip()
+            requests.post("http://localhost:8000/task/",
+                          json={"name": taskName})
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="create success!"))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="something wents wrong, please retry..."))
+    elif "get id" in event.message.text:
+        try:
+            response = requests.get("http://localhost:8000/task/")
+            res = ""
+            for item in response.json():
+                res += item['_id'] + '\n'
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text=res))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text="something wents wrong, please retry..."))
+    elif "help" in event.message.text:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=helpMessage))
+    # else:
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
